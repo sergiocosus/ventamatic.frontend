844fa298689c2f8d2f79670af79a54708c71afac
Sergio Contreras Sustaita <sergio.contreras@triolabs.mx>
2016-05-01 00:12:03 -0500

    Added homepage

--- src/client/app/+home-page/home-page.component.ts
+++ src/client/app/+home-page/home-page.component.ts
@@ -1,11 +1,12 @@
-import {Component, OnInit} from 'angular2/core';
+import {Component, OnInit, ViewEncapsulation} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 
 @Component({
   moduleId: __moduleName,
   selector: 'home-page',
   templateUrl: 'home-page.component.html',
-  styleUrls: ['home-page.component.css'],
+  // styleUrls: ['home-page.component.css'],
+  encapsulation: ViewEncapsulation.None,
   directives: [ROUTER_DIRECTIVES]
 })
 export class HomePageComponent implements OnInit {
