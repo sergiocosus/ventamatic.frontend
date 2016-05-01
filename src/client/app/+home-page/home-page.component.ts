57b104f6d2cbdde6ae0cefe1d284280059564fb6
Sergio Contreras Sustaita <sergio.contreras@triolabs.mx>
2016-05-01 00:38:45 -0500

    Added logo and improve HomePage Template

--- src/client/app/+home-page/home-page.component.ts
+++ src/client/app/+home-page/home-page.component.ts
@@ -7,10 +7,27 @@ import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router'
   templateUrl: 'home-page.component.html',
   // styleUrls: ['home-page.component.css'],
   encapsulation: ViewEncapsulation.None,
-  directives: [ROUTER_DIRECTIVES]
+  directives: [ROUTER_DIRECTIVES,
+  ]
 })
 export class HomePageComponent implements OnInit {
 
+  options = [
+      'Ventas',
+      'Compras',
+      'Inventario',
+      'Producto',
+      'Clientes',
+      'Provedores',
+      'User',
+      'Permisos',
+      'Sucursales',
+      'Reportes',
+  ];
+
+  username = "Pepe pequitas";
+  time = "8:00pm";
+    
   constructor() {}
   
   ngOnInit() {
