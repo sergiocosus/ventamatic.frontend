5c6b267c53858d6bb7cbdcc15902d01c37c97041
Sergio Contreras Sustaita <sergio.contreras@triolabs.mx>
2016-05-22 15:44:12 -0500

    Add icons to navbar

--- src/app/+app/shared/aside-nav/aside-nav.component.ts
+++ src/app/+app/shared/aside-nav/aside-nav.component.ts
@@ -12,43 +12,53 @@ export class AsideNavComponent implements OnInit {
   public routes = [
     {
       name: 'Venta',
-      path: '/app/venta'
+      path: '/app/venta',
+      icon: 'fa-cart-arrow-down'
     },
     {
       name: 'Compra',
-      path: '/app/compra'
+      path: '/app/compra',
+      icon: 'fa-shopping-cart'
     },
     {
       name: 'Inventario',
-      path: '/app/inventario'
+      path: '/app/inventario',
+      icon: 'fa-list-ul'
     },
     {
       name: 'Productos',
-      path: '/app/productos'
+      path: '/app/productos',
+      icon: 'fa-shopping-bag'
     },
     {
       name: 'Clientes',
-      path: '/app/clientes'
+      path: '/app/clientes',
+      icon: 'fa-user'
     },
     {
       name: 'Proveedores',
-      path: '/app/proveedores'
+      path: '/app/proveedores',
+      icon: 'fa-user-secret'
     },
     {
       name: 'Usuarios',
-      path: '/app/usuarios'
+      path: '/app/usuarios',
+      icon: 'fa-users'
     },
     {
       name: 'Roles',
-      path: '/app/roles'
+      path: '/app/roles',
+      icon: 'fa-lock'
     },
     {
       name: 'Sucursales',
-      path: '/app/sucursales'
+      path: '/app/sucursales',
+      icon: 'fa-industry'
     },
     {
       name: 'Reportes',
-      path: '/app/reportes'
+      path: '/app/reportes',
+      icon: 'fa-line-chart'
     }
   ];
 
